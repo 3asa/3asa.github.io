af5045a5c6fed3868ff6f13e3fc82294f785f908
tiziodcaio <tiziodcaio@gmail.com>
2021-04-12 11:28:49 +0200

    Remove trashes

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,9 +4,7 @@ module.exports = {
   title: 'Portale 2asa',
   description: description,
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }]
   ],
   themeConfig: {
     editLink: false,
