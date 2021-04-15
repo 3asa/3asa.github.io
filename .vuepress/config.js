a88a46be91d4e9dbf2c7d5d09cf6838b781beb4c
tiziodcaio <tiziodcaio@gmail.com>
2021-04-15 13:52:05 +0200

    Re add medium zoom

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -28,7 +28,7 @@ module.exports = {
     }
   },
   plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
+    '@vuepress/back-to-top',
+    '@vuepress/medium-zoom',
   ]
 }
